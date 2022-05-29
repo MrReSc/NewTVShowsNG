98190a3208af1cb5695a4c069e5beefdd78485d6
MrReSc <resc@mailbox.org>
2022-05-29 07:03:39 +0200

    html css

--- newTvShow.py
+++ newTvShow.py
@@ -9,7 +9,7 @@ import os.path
 from urllib.parse import urlparse
 from datetime import datetime as dt
 
-DEBUG = False
+DEBUG = True
 
 JELLY_API_KEY = os.environ["JELLY_API_KEY"]
 JELLY_USER_ID = os.environ["JELLY_USER_ID"]
@@ -144,7 +144,7 @@ for feed in feeds:
 ##############################################################################
 # Daten speichern
 ##############################################################################
-# Wenn die Liste zu lamg wird, wird solange das älteste Element entfernt bis die Liste nicht mehr zu Gross ist
+# Wenn die Liste zu lang wird, wird solange das älteste Element entfernt bis die Liste nicht mehr zu Gross ist
 while len(showsRSS) > MAX_COUNT:
     showsRSS.pop(0)
 
@@ -162,23 +162,40 @@ except:
 showsRSS = sorted(showsRSS, key=lambda d: d['Published'], reverse=True) 
 
 # HTML generieren
+BG_COLOR   = "#1e1e1e"
+FG_COLOR   = "#ffffff"
+BORDER     = "#3f3f3f"
+HEADER     = "#333333"
+LINK       = "#007acc"
+V_LINK     = "#c58af9"
+SEASON     = "#242424"
+SEASON_NEW = "#b73db7"
+
 strStyle = '''<style> 
-table, th, td { border: 1px solid white; border-collapse: collapse; } 
-input[type="text"], textarea { background-color : #1e1e1e; color: #ffffff; }
+table, td { border: 1px solid ''' + BORDER + '''; border-collapse: collapse; } 
+th { border: 1px solid ''' + BORDER + '''; border-collapse: collapse; background-color : '''+ HEADER +''';} 
+input[type="text"] { background-color : '''+ BG_COLOR +'''; color: ''' + FG_COLOR + '''; margin-bottom: 25px; padding: 5px;}
+body { font-family: 'Courier New', monospace; font-size: 120%;}
 </style>'''
-strBodyStyle = 'bgcolor="#1e1e1e" text="#ffffff" link="#5f78a1" vlink="#c58af9" alink="#5f78a1"'
+strBodyStyle = 'bgcolor="' + BG_COLOR + '" text="' + FG_COLOR + '" link="' + LINK + '" vlink="' + V_LINK + '" alink="' + LINK + '"'
 strInput = '<input type="text" id="myInput" onkeyup="myFunction()" placeholder="nach Qualitaet suchen...">'
 strHeader = '<table id="myTable"><tr><th>Titel</th><th>S</th><th>E</th><th>SJ</th><th>EJ</th><th>Link</th><th>Datum</th><th>Qualitaet</th></tr>'
-strTable = "<html><head>" + strStyle + '</head><body ' + strBodyStyle + '>' + strInput + strHeader
+strTable = "<!DOCTYPE html><html><head>" + strStyle + '</head><body ' + strBodyStyle + '>' + strInput + strHeader
  
 for show in showsRSS:
     domain = urlparse(show["Link"]).netloc
-    strRW = '<tr><td>' + show["Name"] + '</td><td width="50" bgcolor="#682d68" style="text-align:center">'\
-                       + str(show["Season"] or '') + '</td><td width="50" bgcolor="#682d68" style="text-align:center">'\
-                       + str(show["Episode"] or '') + '</td><td width="50" style="text-align:center">'\
-                       + str(show["SeasonJellyfin"] or '') + '</td><td width="50" style="text-align:center">'\
+    if show["Season"] <= show["SeasonJellyfin"]:
+       SEASON_NEW = BG_COLOR 
+    else:
+        if show["EpisodeJellyfin"] <= show["Episode"]:
+            SEASON_NEW = BG_COLOR
+
+    strRW = '<tr><td>' + show["Name"][:100] + '</td><td width="50" bgcolor="' + SEASON +' " style="text-align:center">'\
+                       + str(show["Season"] or '') + '</td><td width="50" bgcolor="' + SEASON + '" style="text-align:center">'\
+                       + str(show["Episode"] or '') + '</td><td width="50" bgcolor="' + SEASON_NEW + '" style="text-align:center">'\
+                       + str(show["SeasonJellyfin"] or '') + '</td><td width="50" bgcolor="' + SEASON_NEW + '" style="text-align:center">'\
                        + str(show["EpisodeJellyfin"] or '') + '</td><td>'\
-                       + '<a href="' + show["Link"] + '" target="_blank">'+ domain +'</a></td><td>'\
+                       + '<a href="' + show["Link"] + '" target="_blank" title="' + show["Link"] + '">'+ domain +'</a></td><td>'\
                        + str(show["Published"] or '') + '</td><td style="text-align:center">'\
                        + str(show["Quality"] or '') + '</td></tr>' 
 
